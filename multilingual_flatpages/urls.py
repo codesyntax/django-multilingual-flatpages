94eb2d034bf84be992c488c5ee9dda9708860685
Urtzi Odriozola <uodriozola@codesyntax.com>
2016-09-21 14:46:50 +0200

    Translatable view

--- multilingual_flatpages/urls.py
+++ multilingual_flatpages/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from multilingual_flatpages import views
 
 urlpatterns = [
-    url(r'^(?P<url>.*)$', views.flatpage, name='multilingual_flatpages.views.flatpage'),
+    url(r'^(?P<url>.*)$', views.flatpage, name='multilingual_flatpages'),
 ]
