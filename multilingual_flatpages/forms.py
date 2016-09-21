8adc6cce14d543ef7dcb79fa890002d69b5c7f42
Urtzi Odriozola <uodriozola@codesyntax.com>
2016-09-21 13:28:54 +0200

    Translatable form

--- multilingual_flatpages/forms.py
+++ multilingual_flatpages/forms.py
@@ -32,7 +32,7 @@ class FlatpageForm(TranslatableModelForm):
             )
         if (settings.APPEND_SLASH and (
                 'django.middleware.common.CommonMiddleware' in settings.MIDDLEWARE_CLASSES) and
-                not url.endswith('/'):
+                not url.endswith('/')):
             raise forms.ValidationError(
                 ugettext("URL is missing a trailing slash."),
                 code='missing_trailing_slash',
