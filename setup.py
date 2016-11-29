cd093a4f58401ff336b88c9c5a6ea32dbee60588
Urtzi Odriozola <uodriozola@codesyntax.com>
2016-11-29 12:47:32 +0100

    Back to development: 0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(source):
     required = set([str(ir.req) for ir in install_reqs])
     return required
 
-version = '0.2'
+version = '0.3.dev0'
 
 setup(name='multilingual_flatpages',
       version=version,
