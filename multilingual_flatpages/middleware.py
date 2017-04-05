46346b87b6c0d3dd82834e87713360a86a071b2f
Mikel Larreategi <mlarreategi@codesyntax.com>
2017-04-05 11:14:24 +0200

    cosmetics

--- multilingual_flatpages/middleware.py
+++ multilingual_flatpages/middleware.py
@@ -3,17 +3,17 @@ from django.http import Http404
 from multilingual_flatpages.views import flatpage
 
 try:
-    from django.utils.deprecation import MiddlewareMixin
-    object = MiddlewareMixin
+    from django.utils.deprecation import MiddlewareMixin as Base
 except ImportError:
-    pass
+    Base = object
 
 
-class FlatpageFallbackMiddleware(object):
+class FlatpageFallbackMiddleware(Base):
 
     def process_response(self, request, response):
         if response.status_code != 404:
-            return response  # No need to check for a flatpage for non-404 responses.
+            # No need to check for a flatpage for non-404 responses.
+            return response
         try:
             return flatpage(request, request.path_info)
         # Return the original response if any errors happened. Because this
