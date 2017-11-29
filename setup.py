665cca1b7636ca1d48d1c72fb07c015760ec24ff
Mikel Larreategi <mlarreategi@codesyntax.com>
2017-11-29 08:52:22 +0100

    Back to development: 0.8.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(source):
     required = set([str(ir.req) for ir in install_reqs])
     return required
 
-version = '0.8.1'
+version = '0.8.2.dev0'
 
 setup(name='multilingual_flatpages',
       version=version,
